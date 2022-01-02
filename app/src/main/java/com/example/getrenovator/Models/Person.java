218c610c7edbccd68a74df364e29d2efd0ebcc9f
tomermz <tomermz20@gmail.com>
2022-01-02 12:04:18 +0200

    Initial Commit

--- app/src/main/java/com/example/getrenovator/Models/Person.java
+++ app/src/main/java/com/example/getrenovator/Models/Person.java
@@ -5,7 +5,7 @@ public class Person {
     String first_name;
     String last_name;
     String mail;
-    String pass;
+    String password;
     String phone;
     UserType userType;
 
@@ -44,12 +44,12 @@ public class Person {
         this.mail = mail;
     }
 
-    public String getPass() {
-        return pass;
+    public String getPassword() {
+        return password;
     }
 
-    public void setPass(String pass) {
-        this.pass = pass;
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public String getPhone() {
